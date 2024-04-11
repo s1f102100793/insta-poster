1e77e6ad7aa2a58f18fd964d1f7df4150237e3e7
s1f102100793 <s1f102100793@iniad.org>
2024-04-11 12:35:41 +0900

    update: mergeImages use Math.round to canvas.composite

--- server/src/sharp/index.ts
+++ server/src/sharp/index.ts
@@ -1,36 +1,50 @@
 import sharp from "sharp";
 
+interface sharpMetadata {
+  width: number;
+  height: number;
+}
+
 export const sharpUtils = {
-  async mergeImages (image1: File, screenshot: File, outputPath: string) {
+  async mergeImages(image1: File, screenshot: File, outputPath: string) {
     const image1Buffer = await image1.arrayBuffer();
     const screenshotBuffer = await screenshot.arrayBuffer();
 
     const sharpImage1 = sharp(image1Buffer);
     const sharpScreenshot = sharp(screenshotBuffer);
 
-    const image1Metadata = await sharpImage1.metadata();
-    const screenshotMetadata = await sharpScreenshot.metadata();
+    const image1Metadata = await sharpImage1.metadata() as sharpMetadata;
+    const screenshotMetadata = await sharpScreenshot.metadata() as sharpMetadata;
 
-    const newWidth = image1Metadata.width as number + screenshotMetadata.width as number;
-    const newHeight = Math.max(image1Metadata.height as number, screenshotMetadata.height as number);
-    const newSideLength = Math.max(image1Metadata.height, screenshotMetadata.height);
+    const unifiedWidth = Math.max(image1Metadata.width, screenshotMetadata.width);
+    const canvasSideLength = unifiedWidth * 2;
 
-    const image1Offset = Math.floor((newSideLength - image1Metadata.height) / 2);
-    const screenshotOffset = Math.floor((newSideLength - screenshotMetadata.height) / 2);
+    const resizedImage1Buffer = await sharpImage1.resize({ width: unifiedWidth }).toBuffer();
+    const resizedScreenshotBuffer = await sharpScreenshot.resize({ width: unifiedWidth }).toBuffer();
+
+    const resizedImage1Metadata = await sharp(resizedImage1Buffer).metadata() as sharpMetadata;
+    const resizedScreenshotMetadata = await sharp(resizedScreenshotBuffer).metadata() as sharpMetadata;
+
+    console.log("resizedImage1 size", resizedImage1Metadata.width, resizedImage1Metadata.height);
+    console.log("resizedScreenshot size", resizedScreenshotMetadata.width, resizedScreenshotMetadata.height);
 
     const canvas = sharp({
       create: {
-        width: newSideLength * 2,
-        height: newSideLength,
+        width: canvasSideLength,
+        height: canvasSideLength,
         channels: 4,
-        background: { r: 255, g: 255, b: 255, alpha: 1 } // 白背景
+        background: { r: 255, g: 255, b: 255, alpha: 1 }
       }
     });
 
+    const resizedImage1BufferTop = Math.round((canvasSideLength - resizedImage1Metadata.height) / 2);
+    const resizedScreenshotBufferTop = Math.round((canvasSideLength - resizedScreenshotMetadata.height) / 2);
+
     const compositeImage = canvas.composite([
-      { input: await sharpImage1.extract({ width: image1Metadata.width / 2, height: image1Metadata.height, left: image1Metadata.width / 4, top: 0 }).toBuffer(), left: newSideLength, top: image1Offset },
-      { input: await sharpScreenshot.extract({ width: screenshotMetadata.width / 2, height: screenshotMetadata.height, left: 0, top: 0 }).toBuffer(), left: 0, top: screenshotOffset }
+      { input: resizedImage1Buffer, left: 0, top: resizedImage1BufferTop },
+      { input: resizedScreenshotBuffer, left: unifiedWidth, top: resizedScreenshotBufferTop }
     ]);
+
     try {
       await compositeImage.toFile(outputPath);
     } catch (error) {
