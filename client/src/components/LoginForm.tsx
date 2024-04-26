d675ae792a6e39e44b050a700b66713170bddd7d
s1f102100793 <s1f102100793@iniad.org>
2024-04-26 18:27:47 +0900

    update: LoginForm component styling

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,5 +1,6 @@
-// LoginForm.jsx
-import React, { useState } from "react";
+import { useState } from "react";
+import { TextInput } from "./TextInput";
+import { Button } from "./Button";
 
 interface LoginFormProps {
   onLogin: (username: string, password: string) => void;
@@ -9,31 +10,24 @@ function LoginForm({ onLogin }: LoginFormProps) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    onLogin(username, password);
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+    <div className="flex h-screen justify-center items-center">
+      <div className="flex flex-col border-white border-2 px-8 pt-6 pb-8 mb-4 w-72 gap-6">
+        <label>
+          Username:
+          <TextInput value={username} setValue={setUsername} />
+        </label>
+        <label>
+          Password:
+          <TextInput value={password} setValue={setPassword} type="password" />
+        </label>
+        <Button
+          color="bg-blue-500"
+          text="Login"
+          onClick={() => onLogin(username, password)}
         />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <button type="submit">Login</button>
-    </form>
+      </div>
+    </div>
   );
 }
 
