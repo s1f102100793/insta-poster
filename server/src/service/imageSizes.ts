a71edab9369f86a8e354ee3af3016e7e1e62202e
s1f102100793 <s1f102100793@iniad.org>
2024-06-26 00:48:14 +0900

    add: imageSizes to service

---
+++ server/src/service/imageSizes.ts
@@ -0,0 +1,17 @@
+export const paddingSize = 100;
+
+export type ImageSizeType = {
+  width: number;
+  height: number;
+};
+
+export const ImageSizes = {
+  unifiedScreenshotSize: {
+    width: 870,
+    height: 1882,
+  },
+  iphoneMockupBaseSize: {
+    width: 850 + 2 * paddingSize,
+    height: 1850 + 2 * paddingSize,
+  },
+};
